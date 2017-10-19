ac96e677bb56376680f2f3b2f0a38285fa1801ed
sadiqultra <sadikultra@gmail.com>
2017-10-19 22:53:42 +0600

    API

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,9 @@ import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
+import { Ionicons } from '@expo/vector-icons'
+import TextButton from './TextButton'
+import { submitEntry, removeEntry } from './utils/api'
 
 function SubmitBtn({onPress}) {
   return (
@@ -70,8 +73,33 @@ export default class extends Component {
     // Clearn local notification
   }
 
+  reset = () => {
+    const key = timeToString()
+
+    // Update Redux
+
+    // Route to home
+
+    // Update "DB"
+  }
+
   render() {
     const metaInfo = getMetricMetaInfo()
+
+    if(this.props.alreadyLooged) {
+      return (
+        <View>
+          <Ionicons
+            name='ios-happy-outline'
+            size={100}
+          />
+          <Text>You already loged your information for today</Text>
+          <TextButton onPress={this.reset}>
+            Reset
+          </TextButton>
+        </View>
+      )
+    }
 
     return (
       <View>
